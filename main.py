56854cc38da199d4353fbba45afd4977d6d4c0e9
Indu <indira.duranp@gmail.com>
2025-04-15 17:44:20 +0200

    refactor: add custom message to time response

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from datetime import datetime
 
 from fastapi import FastAPI
 from models import Customer, CustomerCreate, Transaction, Invoice
-from db import SessionDep
+from db import SessionDep, create_all_tables
+from sqlmodel import select
 
-app = FastAPI()
+app = FastAPI(lifespan= create_all_tables)
 
 
 # Root endpoint returns a simple welcome message
@@ -16,21 +17,26 @@ async def root():
 
 # Dictionary mapping ISO country codes to timezone strings
 country_timezones = {
-    "CO": "America/Bogota",
-    "MX": "America/Mexico_City",
-    "AR": "America/Argentina/Buenos_Aires",
-    "BR": "America/Sao_Paulo",
-    "PE": "America/Lima",
+    "CO": {"timezone": "America/Bogota", "name": "Colombia"},
+    "MX": {"timezone": "America/Mexico_City", "name": "México"},
+    "AR": {"timezone": "America/Argentina/Buenos_Aires", "name": "Argentina"},
+    "BR": {"timezone": "America/Sao_Paulo", "name": "Brasil"},
+    "PE": {"timezone": "America/Lima", "name": "Perú"},
 }
 
 
 # Endpoint to return the current time based on ISO country code
 @app.get("/time/{iso_code}")
 async def time(iso_code: str):
-    iso = iso_code.upper()  # Convert input to uppercase for consistency
-    timezone_str = country_timezones.get(iso)
-    tz = zoneinfo.ZoneInfo(timezone_str)
-    return{"time": datetime.now(tz)}
+    iso = iso_code.upper()
+    data = country_timezones.get(iso)
+    tz = zoneinfo.ZoneInfo(data["timezone"])
+    now = datetime.now(tz)
+
+    return {
+        "message": f"Estás viendo la hora actual en {data['name']}.",
+        "time": now.strftime("%Y-%m-%d %H:%M:%S")
+    }
 
 
 # In-memory customer storage (simulates a database)
@@ -41,15 +47,17 @@ db_customers: list[Customer] = []
 @app.post("/customers", response_model = Customer)
 async def create_customer(customer_data: CustomerCreate, session: SessionDep):
     customer = Customer.model_validate(customer_data.model_dump())
-    customer.id = len(db_customers)  # Simulate auto-incremented ID
-    db_customers.append(customer)
+    session.add(customer)
+    session.commit()
+    session.refresh(customer)
     return customer
 
 
 # Endpoint to list all registered customers
 @app.get("/customers", response_model=list[Customer])
-async def list_customer():
-    return db_customers
+async def list_customer(session: SessionDep):
+    return session.exec(select(Customer).all())
+    
 
 
 # Endpoint to create a transaction
